3e68966caa4ab1f3aca5011dea859de55bbaa9e6
pphatt <vutienphat0809@gmail.com>
2024-10-27 12:48:30 +0700

    feat: update metro config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,19 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    //nodeModulesPaths: [...localPackagePaths], // update to resolver
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  //watchFolders: [...localPackagePaths],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
